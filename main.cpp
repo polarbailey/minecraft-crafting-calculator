07043e76be07bfc9d699bd56cb7c5a888b4b2488
polarbailey <polarbaileysm@gmail.com>
2026-03-06 21:29:15 +0000

    finished Stack converter

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
@@ -17,11 +19,27 @@ void displayQuantity(string itemName, unsigned int itemAmount) {
     unsigned int excess = remainder % 64; // amount of loose items
 
     cout << "For " << itemAmount << " " << itemName << ", you will need " << endl;
+    this_thread::sleep_for(chrono::milliseconds(500));
+    cout << "\nConverting";
+    cout.flush();
+    this_thread::sleep_for(chrono::milliseconds(500));
+    for (int i = 0; i < 5; i++) {
+        cout << " . ";
+        cout.flush();
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+    cout << endl;
+    if (doubleChests > 0) {
+        cout << "\n" << doubleChests;
+        if (doubleChests == 1) cout << " Double Chest of Shulkers";
+        else cout << " Double Chests of Shulkers";
+        if (shulkers > 0 || stacks > 0 || excess > 0) cout << " + "; // this way there aren't erroneous "+" or other stuff if the math was clean whole numbers
+    }
     if (shulkers > 0) {
         cout << shulkers;
         if (shulkers == 1) cout << " Shulker";
         else cout << " Shulkers";
-        if (stacks > 0 || excess > 0) cout << " + "; // this way there aren't erroneous "+" or other stuff if the math was clean whole numbers
+        if (stacks > 0 || excess > 0) cout << " + "; 
     }
     if (stacks > 0) {
         cout << stacks;
@@ -37,6 +55,9 @@ void displayQuantity(string itemName, unsigned int itemAmount) {
 
 int main() {
     string choice;
+    bool running = true;
+
+    while (running) {
     do {
         cout << "Welcome to my Minecraft Converter. Please select an option: " << endl;
         cout << "1. Stack Converter" << endl;
@@ -46,29 +67,43 @@ int main() {
         transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
 
         if (choice == "1" || choice == "stack") {
-            string itemName;
-            unsigned int itemAmount;
+            string again;
+            do{
+                string itemName;
+                unsigned int itemAmount;
 
-            cout << "What item are you converting? ";
-            cin.ignore();
-            getline(cin, itemName);
+                cout << "What item are you converting? ";
+                cin.ignore();
+                getline(cin, itemName);
 
-            cout << "How much " << itemName << " are you converting? ";
-            cin >> itemAmount;
+                cout << "How much " << itemName << " are you converting? ";
+                cin >> itemAmount;
 
-            displayQuantity(itemName, itemAmount);
-            cout << "\nNote: This tool is currently only accurate for items that stack into stacks of 64." << endl;
-        }
+                displayQuantity(itemName, itemAmount);
+                cout << "\nNote: This tool is currently only accurate for items that stack into stacks of 64." << endl;
+
+                cout << "\nWould you like to convert something else? (yes/no):";
+                cin >> again;
+                transform(again.begin(), again.end(), again.begin(), ::tolower);
+
+                if (again == "0" || again == "quit" || again == "stop") {
+                    cout << "Goodbye!" << endl;
+                    running = false;
+                    break;
+                }
+        } while (again == "yes" || again == "y");
+    }
         else if (choice == "2" || choice == "recipe") {
             cout << "This feature is currently under construction. Please check back later." << endl;
         }
         else if (choice == "0" || choice == "quit" || choice == "stop") {
             cout << "Goodbye!" << endl << endl;
+            running = false;
         }
         else {
             cout << "I'm sorry, my database is limited. You must enter the right selection." << endl << endl; // Cheeky I, Robot reference there. Shoutouts if you got it without this comment.
         } 
     } while (choice != "1" && choice != "stack" && choice != "2" && choice != "recipe" && choice != "0" && choice != "quit" && choice != "stop");
-    
+    }
     return 0;
 }
